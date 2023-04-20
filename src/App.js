3e97cca3559cb8210fc4059b4b821a2f24b04317
Facuzogbe <facundozogbe@gmail.com>
2023-04-19 23:16:06 -0300

    initial scafolding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+// import './App.css';
+const todos = [{ text: 'cortar cebolla', completed: false},
+              { text: 'tomar el curso de introduccion a React', completed: false},
+              { text: 'Llorar con la llorona', completed: false},             
+];
+
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+
+    <TodoCounter/>
+    <h2>Has completado 2 de 3 TODOs</h2>
+    <TodoSearch/>
+    <input pplaceholder="Cebolla"/>
+
+    <TodoList>
+      {todos.map(todo => (
+      <TodoItem />
+      ))}
+    </TodoList>
+     
+    <CreateTodoButton/>
+    <button>+</button>
+    </React.Fragment>
+ );
 }
 
 export default App;
