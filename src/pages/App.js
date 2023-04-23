5ad032a5ed11e8be154783b0ffe8fbf238a93f46
Facuzogbe <facundozogbe@gmail.com>
2023-04-23 20:24:07 -0300

    delete coment in html and config identation in app.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,39 +1,37 @@
-import React from 'react';
-import { TodoCounter} from '../components/TodoCounter/TodoCounter'
-import { TodoSearch } from '../components/TodoSearch/TodoSearch';
-import { TodoList } from '../components/TodoList/TodoList';
-import { TodoItem } from '../components/TodoItem/TodoItem';
+import React from "react";
+import { TodoCounter } from "../components/TodoCounter/TodoCounter";
+import { TodoSearch } from "../components/TodoSearch/TodoSearch";
+import { TodoList } from "../components/TodoList/TodoList";
+import { TodoItem } from "../components/TodoItem/TodoItem";
 import { CreateTodoButton } from "../components/CreateTodoButton/CreateTodoButton";
 
 // import './App.css';
 
-const todos =[
-  {text:'Cortar cebolla', completed: true},
-  {text:'Tormar el curso de intro a react', completed: true},
-  {text:'Llorar con la llorona', completed: true}
+const todos = [
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tormar el curso de intro a react", completed: true },
+  { text: "Llorar con la llorona", completed: true },
 ];
 
 function App(props) {
   return (
     <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
 
-    <TodoCounter/>
-    <TodoSearch/>
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </TodoList>
 
-     <TodoList>
-      {todos.map(todo => (
-      <TodoItem
-      key={todo.text} 
-      text={todo.text}
-      completed={todo.completed}
-      />
-      ))}
-    </TodoList>
-     
-    <CreateTodoButton/>
-
+      <CreateTodoButton />
     </React.Fragment>
- );
+  );
 }
 
 export default App;
